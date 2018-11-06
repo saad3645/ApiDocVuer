58acc386caa67a79257808f3578416e54d539f5f
Saad Ahmed <saad.invariant@gmail.com>
2018-11-06 07:46:34 +0600

    Nov 6 update

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="app-main-layout">
     <md-app md-mode="reveal">
-      <Navbar slot="md-app-toolbar"></Navbar>
+      <md-app-toolbar class="md-primary">
+        <span class="md-title">Invariant Telecom</span>
+        <div class="md-toolbar-section-end">
+          <md-button class="md-primary">Create</md-button>
+          <md-button class="md-primary" @click="gotoDocs">Docs</md-button>
+          <md-button class="md-primary" @click="gotoCollections">Collections</md-button>
+          <md-button class="md-primary" @click="gotoTeam">Team</md-button>
+          <md-button class="md-icon-button"><md-icon>account_circle</md-icon></md-button>
+        </div>
+      </md-app-toolbar>
+      <!--<Navbar slot="md-app-toolbar"></Navbar>-->
       <md-app-content>
         <router-view/>
       </md-app-content>
@@ -10,10 +20,10 @@
 </template>
 
 <script>
-import Navbar from '@/components/Navbar'
+// import Navbar from '@/components/Navbar'
 export default {
   name: 'MainLayout',
-  components: {Navbar}
+  // components: {Navbar}
 }
 </script>
 
